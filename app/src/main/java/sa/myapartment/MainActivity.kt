b19ee10521860eb95b8f839eca6eff3ab61fc4c1
sarora <sarora@deltaintech.com>
2019-07-08 01:15:03 +0530

    Added Helpdesk nd Visitors Screen Layout

--- app/src/main/java/sa/myapartment/MainActivity.kt
+++ app/src/main/java/sa/myapartment/MainActivity.kt
@@ -1,12 +1,32 @@
 package sa.myapartment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        property.setOnClickListener {
+
+        }
+        helpdesk.setOnClickListener {
+
+        }
+        events.setOnClickListener {
+
+        }
+        accounts.setOnClickListener {
+
+        }
+        visitors.setOnClickListener {
+
+        }
+        facilities.setOnClickListener {
+
+        }
     }
 }
